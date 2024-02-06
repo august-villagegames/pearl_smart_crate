adc81264abc9568b6114b586f146aebeebe12d67
August Comstock <augustcomstock@Augusts-MBP.lan>
2024-02-05 19:24:23 -0800

    changes to rpi connector

--- lib/utils/rpi_connector.dart
+++ lib/utils/rpi_connector.dart
@@ -5,28 +5,37 @@ import 'package:http/http.dart';
 String serverUrl = 'localhost:8080';
 
 // create function that sends JSON to ip
-// Future<StreamedResponse> sendRequest(Request request) async {
-//   var client = Client();
+Future<StreamedResponse> sendRequest(Request request) async {
+  var client = Client();
+  try {
+    return await client.send(request);
+  } catch (e) {
+    if (e is ClientException) {
+      throw Exception('Failed to send request: ${e.message}');
+    }
+    throw Exception(e);
+  } finally {
+    client.close();
+  }
+}
 
-//   print('attempting to send request: $request');
-//   try {
-//     return await client.send(request);
-//   } catch (e) {
-//     if (e is ClientException) {
-//       throw Exception('Failed to send request: ${e.message}');
-//     }
-//     throw Exception(e);
-//   } finally {
-//     client.close();
-//   }
-// }
-
+//TODO: try this with ios or simulator instead of chrome
 void encodeJsonPushAll(Map<String, bool> weekdays, TimeOfDay time) async {
-  Request request = Request('Post', Uri.http(serverUrl));
+  Request request = Request('POST', Uri.http(serverUrl)); 
   request.headers['Content-Type'] = 'application/json';
   request.body = json.encode([
     weekdays,
     {'time': "${time.hour}:${time.minute}"}
   ]);
-  print(request.body.toString());
+  print('headers: ${request.headers}');
+  print('body: ${request.body}');
+  print('Url: ${request.url}');
+  print('encoding: ${request.encoding}');
+  print('method: ${request.method}');
+  sendRequest(request);
+  try {
+    jsonDecode(request.body);
+  } catch (e) {
+    print('The provided string is not valid JSON: ${request.body}');
+  }
 }
