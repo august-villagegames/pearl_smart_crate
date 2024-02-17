a330c9e3fc289bf872e30c0b941bdb2e521b2f2c
August Comstock <augustcomstock@Augusts-MBP.lan>
2024-02-17 13:28:02 -0800

    change ip

--- lib/utils/rpi_connector.dart
+++ lib/utils/rpi_connector.dart
@@ -2,8 +2,7 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart';
 
-String serverUrl =
-    '10.0.0.69'; //find out how to get to specific process/folder running on the ngrok tunnel
+String serverUrl = 'pearlsmartcrate.duckdns.org';
 
 // create function that sends JSON to ip
 Future<StreamedResponse> sendRequest(Request request) async {
